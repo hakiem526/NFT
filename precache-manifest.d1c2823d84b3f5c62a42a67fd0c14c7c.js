0df0e9e7da8e7fad31dcd6a95703974b8168375a
hakiem526 <hakiemm@hotmail.com>
2022-12-03 14:29:04 +0800

    Updates

---
+++ precache-manifest.d1c2823d84b3f5c62a42a67fd0c14c7c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1997e235da7d9045b2423c66335430e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72ff4972fc6e37bb8297",
+    "url": "/static/css/main.15b3af49.chunk.css"
+  },
+  {
+    "revision": "c219a5812f75394faa48",
+    "url": "/static/js/2.c1c2f1a4.chunk.js"
+  },
+  {
+    "revision": "72ff4972fc6e37bb8297",
+    "url": "/static/js/main.d8ede305.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  },
+  {
+    "revision": "f3fc160d6b96242834df15839333dc6c",
+    "url": "/static/media/build.f3fc160d.png"
+  },
+  {
+    "revision": "dca4ad5b6fec7cd89e40a1c933b66cf0",
+    "url": "/static/media/educational.dca4ad5b.png"
+  },
+  {
+    "revision": "64cd5216bb0dfe1d3936c72b8217a4fd",
+    "url": "/static/media/home.64cd5216.png"
+  },
+  {
+    "revision": "872756dc29cf88ea3510e6043b9e96f2",
+    "url": "/static/media/lab.872756dc.png"
+  },
+  {
+    "revision": "c08841d34882dbf956a0d510a7698ecd",
+    "url": "/static/media/marketplace.c08841d3.png"
+  },
+  {
+    "revision": "81b7e23d636b262bdb17253827aa6208",
+    "url": "/static/media/quandefy_logo_nobg.81b7e23d.png"
+  },
+  {
+    "revision": "09098e34dbd57612a916dda63495f8cd",
+    "url": "/static/media/stake.09098e34.png"
+  },
+  {
+    "revision": "384517d50a6d763cffb89dbe03386b82",
+    "url": "/static/media/tools.384517d5.png"
+  }
+]);
